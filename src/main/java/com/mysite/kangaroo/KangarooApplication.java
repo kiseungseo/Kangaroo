d5a2cdac6a760f5d8025703594b317ba7554b0e2
kiseungseo <rltmdtj@naver.com>
2024-01-22 12:30:51 +0900

    주석

---
+++ src/main/java/com/mysite/kangaroo/KangarooApplication.java
@@ -0,0 +1,13 @@
+package com.mysite.kangaroo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KangarooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KangarooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mysite/kangaroo/KangarooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysite.kangaroo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KangarooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
