dd197dd53c8d28fc48d1a07f1fbc75885caddba0
kiseungseo <rltmdtj@naver.com>
2024-02-06 22:41:59 +0900

    프로필 및 로그인

--- src/main/java/com/mysite/kangaroo/HomeController.java
+++ src/main/java/com/mysite/kangaroo/HomeController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 public class HomeController {
 	
+	//메인로그인 페이지
 	@RequestMapping("/")
     public String home() {
         return "redirect:/user/login";
